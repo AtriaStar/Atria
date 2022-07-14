f3a4751fc80e1c8e539b507ed8d68ecf4c7f3b79
jonas6101 <66283437+jonas6101@users.noreply.github.com>
2022-07-14 11:46:22 +0200

    Add authentication controller

---
+++ Atria/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Text;
+
+namespace Atria.Controllers
+{
+
+    [ApiController]
+    [Route("api/")]
+    public class AuthenticationController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public AuthenticationController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            return NotFound("User not found");
+        }
+
+
+    }
+}
