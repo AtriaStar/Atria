cc4b2059dc222cbda518f31449285bbe007fa090
JohHub <j.a.huber@t-online.de>
2022-07-31 12:43:14 +0200

    Add models for databinding after frontend merge

---
+++ Models/Registration.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models; 
+
+public class Registration {
+    
+    [Required]
+    public string FirstName { get; set; }
+    
+    [Required]
+    public string LastName { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    public string Password { get; set; }
+}
