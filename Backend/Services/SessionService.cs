1e42ea5fa727afaf3d9458f87be32e702c3c9495
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-08-17 18:52:22 +0200

    Improve cookies

--- Backend/Services/SessionService.cs
+++ Backend/Services/SessionService.cs
@@ -27,10 +27,9 @@ public class SessionService {
             IsEssential = true,
             HttpOnly = true,
             Secure = true,
-            Domain = "localhost",
             MaxAge = ExpireDuration,
-            SameSite = SameSiteMode.None, // TODO: Reinvestigate, I fear this is necessary
-            //Path = "/api/", // TODO: Implement
+            SameSite = SameSiteMode.Strict,
+            Path = "/api/",
         });
     }
 
