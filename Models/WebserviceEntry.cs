27e59ee43483b7d5f60e4f274fa7b8ea454634aa
2hyunz <74467584+uwlnf@users.noreply.github.com>
2022-08-17 20:48:40 +0200

    Edit model binding in NewWse

--- Models/WebserviceEntry.cs
+++ Models/WebserviceEntry.cs
@@ -4,18 +4,35 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Models; 
 
 public class WebserviceEntry {
+
     [Key]
+    [Required]
     public string Name { get; set; } = null!;
+
+    [Required]
     public string ShortDescription { get; set; } = null!;
+
+    [Required]
+    [Url]
     public Uri Link { get; set; } = null!;
     public string FullDescription { get; set; } = null!;
+
+    [Url]
     public Uri DocumentationLink { get; set; } = null!;
-    public string Changelog { get; set; } = null!;
+
+    public string Documentation { get; set; } = null!;
+    public string ChangeLog { get; set; } = null!;
     public int ViewCount { get; set; }
     public DateTimeOffset CreationDate { get; set; }
     public User ContactPerson { get; set; } = null!;
     public ICollection<Question> Questions { get; set; } = null!;
+
+    [MaxLength(20)]
     public ICollection<Tag> Tags { get; set; } = null!;
+
+    [RegularExpression(@"^[a-zA-Z0-9-.]*(,[a-zA-Z0-9-.]+)*$", ErrorMessage = "Tags are not separated with a comma.")]
+    public string NewTags { get; set; } = null!;
+
     public ICollection<Review> Reviews { get; set; } = null!;
     public ICollection<Collaborator> Collaborators { get; set; } = null!;
 }
