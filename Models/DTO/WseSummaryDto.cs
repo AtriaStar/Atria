5fb5979d93750108a055c1c164c487824c3a54bf
JohHub <j.a.huber@t-online.de>
2022-08-20 19:25:01 +0200

    not null declaration

--- Models/DTO/WseSummaryDto.cs
+++ Models/DTO/WseSummaryDto.cs
@@ -3,14 +3,14 @@
 public class WseSummaryDto {
     
     public long Id { get; set; }
-    public string Name { get; set; }
-    public string ShortDescription { get; set; }
-    public ICollection<Tag> Tags { get; set; }
+    public string Name { get; set; } = null!;
+    public string ShortDescription { get; set; } = null!;
+    public ICollection<Tag> Tags { get; set; } = null!;
     public DateTimeOffset CreationDate { get; set; }
     public bool IsOnline { get; set; }
     public double AverageRating { get; set; }
     public int ViewCount { get; set; }
-    public Uri Link { get; set; }
-    public bool IsBookmark { get; set; } = false;
+    public Uri Link { get; set; } = null!;
+    public bool IsBookmark { get; set; }
 
 }
