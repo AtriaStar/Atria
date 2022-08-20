0f3e6e0053d29d8c42ed34e53dc762c942da0d8e
jonas6101 <66283437+jonas6101@users.noreply.github.com>
2022-08-20 02:12:42 +0200

    Fix cors issue

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,17 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-        policy =>
-        {
-            policy.WithOrigins("https://localhost:44368/login",
-                "https://localhost:44368"
-                );
-        });
-});
-
 // Add services to the container.
 builder.Services.AddControllers(options => options.UseCentralRoutePrefix(new RouteAttribute("api")))
     .AddJsonOptions(options => options.JsonSerializerOptions.WriteIndented = builder.Environment.IsDevelopment());
@@ -35,7 +24,7 @@ if (app.Environment.IsDevelopment()) {
     app.UseSwaggerUI();
 }
 app.UseRouting();
-app.UseCors(MyAllowSpecificOrigins);
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
 app.UseHttpsRedirection();
 app.MapControllers();
