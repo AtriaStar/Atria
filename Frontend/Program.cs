72467de7b7b86545e511b4afd45fb4e06b03b681
JohHub <j.a.huber@t-online.de>
2022-08-21 15:13:47 +0200

    Implemented BaseAddress according to #19 with login adjusted for temporary MWE

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["BaseAddress"] ?? throw new InvalidOperationException("no backend address provided")) });
 builder.Services.AddHttpClient();
 await builder.Build().RunAsync();
