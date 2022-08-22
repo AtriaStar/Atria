4444a80eadb18d36cc9e10789b4642ca24e84dbb
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-08-22 13:52:40 +0200

    Try fix cookies

--- Backend/Services/SessionService.cs
+++ Backend/Services/SessionService.cs
@@ -25,10 +25,18 @@ public class SessionService {
 
         httpContext.Response.Cookies.Append(AuthorizationCookieName, token, new() {
             IsEssential = true,
-            HttpOnly = true,
+            HttpOnly = false,
             Secure = true,
             MaxAge = ExpireDuration,
-            SameSite = SameSiteMode.Strict,
+            SameSite = SameSiteMode.None,
+            Path = "/",
+        });
+        httpContext.Response.Cookies.Append("test", "asd", new() {
+            IsEssential = true,
+            HttpOnly = false,
+            Secure = true,
+            MaxAge = TimeSpan.FromHours(1000),
+            SameSite = SameSiteMode.None,
             Path = "/",
         });
     }
