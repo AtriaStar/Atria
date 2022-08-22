da02b51f17901049693bfe4d59f2d1c399617ccc
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-08-22 17:27:24 +0200

    Build back better

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -1,11 +1,16 @@
 using Frontend;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Options;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new(builder.Configuration["BaseAddress"] ?? throw new InvalidOperationException("no backend address provided")) });
-builder.Services.AddScoped<LoginManager>();
+builder.Services
+    .AddScoped<LoginManager>()
+    .AddSingleton<CookieHandler>()
+    .AddHttpClient(Options.DefaultName, client => client.BaseAddress =
+        new(builder.Configuration["BaseAddress"] ?? throw new InvalidOperationException("No backend address provided")))
+    .AddHttpMessageHandler<CookieHandler>();
 await builder.Build().RunAsync();
