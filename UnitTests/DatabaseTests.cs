ae1a8c1e883eaa84530c371ff855d0c9166abd6b
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-08-25 03:36:33 +0200

    Add database test

---
+++ UnitTests/DatabaseTests.cs
@@ -0,0 +1,31 @@
+﻿using Backend;
+using DatabaseMocker;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitTests; 
+
+public class DatabaseTests {
+    public DatabaseTests() {
+        using var db = new AtriaContext();
+        var props = typeof(AtriaContext).GetProperties();
+        foreach (var property in typeof(AtriaContext).GetProperties()
+                     .Where(x => x.PropertyType.IsGenericType
+                                 && x.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>))) {
+            var set = (dynamic)property.GetValue(db)!;
+            set.RemoveRange(set);
+        }
+
+        db.SaveChanges();
+    }
+
+    [Fact]
+    public async Task TestDatabase() {
+        await using var db = new AtriaContext();
+        await db.Users.AddRangeAsync(Enumerable.Range(0, 150)
+            .Select(_ => UserMocker.GenerateUser())
+            .DistinctBy(x => x.Email)
+            .Take(100));
+        await db.SaveChangesAsync();
+        Assert.Equal(100, db.Users.Count());
+    }
+}
