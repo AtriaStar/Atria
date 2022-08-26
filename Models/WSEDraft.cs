678e13f57a5894b5ec10c65d94afd2b9435f8337
Jesicca1 <105424817+Jesicca1@users.noreply.github.com>
2022-08-26 11:06:11 +0200

    Add edit abilities on wsedraft

--- Models/WSEDraft.cs
+++ Models/WSEDraft.cs
@@ -6,14 +6,14 @@ namespace Models;
 public class WSEDraft {
     [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
-    public string Name { get; set; } = null!;
-    public string ShortDescription { get; set; } = null!;
+    public string? Name { get; set; } = null!;
+    public string? ShortDescription { get; set; } = null!;
     [Url]
-    public string Link { get; set; } = null!;
-    public string FullDescription { get; set; } = null!;
+    public string? Link { get; set; } = null!;
+    public string? FullDescription { get; set; } = null!;
     [Url]
     public string? DocumentationLink { get; set; } = null!;
     public string? Documentation { get; set; } = null!;
-    public string ChangeLog { get; set; } = null!;
+    public string? ChangeLog { get; set; } = null!;
     public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
 }
