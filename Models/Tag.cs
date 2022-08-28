0cbd8a7feaf55dfdd7887c983cfbe69dff523444
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-08-28 02:50:35 +0200

    Tag controller

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -5,6 +5,7 @@ namespace Models;
 public class Tag {
     [Key]
     public string Name { get; set; } = null!;
-    public DateTimeOffset CreationTime { get; set; }
-    public uint UseCount { get; set; }
+    public string? Description { get; set; }
+    public DateTimeOffset CreationTime { get; set; } = DateTimeOffset.UtcNow;
+    public long UseCount { get; set; }
 }
