118f5722d8e96c036c8961ca8ccd581aa4cce357
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-08-28 04:04:23 +0200

    Selective validation

---
+++ Backend/SelectiveValidator.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Backend; 
+
+public class NonValidatingValidator : IObjectModelValidator {
+    private readonly IObjectModelValidator _baseValidator;
+
+    public NonValidatingValidator(IObjectModelValidator baseValidator) {
+        _baseValidator = baseValidator;
+    }
+
+    public void Validate(ActionContext actionContext, ValidationStateDictionary? validationState, string prefix, object? model) {
+        var preExisting = actionContext.ModelState.Keys.ToArray();
+        _baseValidator.Validate(actionContext, validationState, prefix, model);
+        if (model == null) { return; }
+
+        var ignoredAttributes = new Lazy<HashSet<string>>(() => model.GetType().GetProperties()
+            .Where(x => x.HasAttribute<JsonIgnoreAttribute>())
+            .Select(x => x.Name)
+            .ToHashSet());
+
+        foreach (var key in actionContext.ModelState.Keys.Except(preExisting)) {
+            if (ignoredAttributes.Value.Contains(key)) {
+                actionContext.ModelState.Remove(key);
+            }
+        }
+    }
+}
