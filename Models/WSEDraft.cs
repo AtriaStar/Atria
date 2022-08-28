42353b0d514abfe644e8ece2aaa9093eec8b27be
JohHub <j.a.huber@t-online.de>
2022-08-28 12:58:35 +0200

    further url misvalidation fix

--- Models/WSEDraft.cs
+++ Models/WSEDraft.cs
@@ -4,19 +4,29 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Models;
 
 public class WSEDraft {
+    private string? _documentationLink;
+    private string? _link;
+
     [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
     [Required]
     public string Name { get; set; } = null!;
     public string? ShortDescription { get; set; }
+
     [Url]
-    [DisplayFormat(ConvertEmptyStringToNull = true)]
-    public string? Link { get; set; }
+    public string? Link {
+        get => _link;
+        set => _link = string.IsNullOrEmpty(value) ? null : value;
+    }
+
     public string? FullDescription { get; set; }
 
     [Url]
-    [DisplayFormat(ConvertEmptyStringToNull = true)]
-    public string? DocumentationLink { get; set; }
+    public string? DocumentationLink {
+        get => _documentationLink;
+        set => _documentationLink = string.IsNullOrEmpty(value) ? null : value;
+    }
+
     public string? Documentation { get; set; }
     public string? ChangeLog { get; set; }
 
