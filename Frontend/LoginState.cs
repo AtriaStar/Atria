57f91da08bc4e4663805116f5a909bb911bcc2f8
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-08-28 23:57:54 +0200

    Better login management

---
+++ Frontend/LoginState.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+using Models;
+
+namespace Frontend; 
+
+public class LoginState {
+    public bool Fetched { get; private set; }
+
+    public bool LoggedIn => User != null;
+
+    public User? User { get; private set; }
+
+    public LoginState(HttpClient client) {
+#pragma warning disable CS4014
+        Check(client);
+#pragma warning restore CS4014
+    }
+
+    private async Task Check(HttpClient client) {
+        var cnt = await client.GetAsync("auth");
+        if (cnt.IsSuccessStatusCode) {
+            User = await cnt.Content.ReadFromJsonAsync<User>();
+        }
+        Fetched = true;
+    }
+}
