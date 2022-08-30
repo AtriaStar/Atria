843ee9d9c1e3a3a907513241972c444868a4a533
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-08-30 06:37:26 +0200

    Password and ConfirmPassword must match; Fixes #101

--- Models/DTO/Registration.cs
+++ Models/DTO/Registration.cs
@@ -17,9 +17,7 @@ public class Registration {
     [DataType(DataType.Password)]
     [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
     public string Password { get; set; } = null!;
-
-    [Required]
-    [DataType(DataType.Password)]
-    [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+    
+    [CompareProperty(nameof(Password))]
     public string ConfirmPassword { get; set; } = null!;
 }
