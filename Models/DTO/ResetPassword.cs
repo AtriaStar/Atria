ff8ab067f5bdbc6de1dfdac2da6a57379be9bcf6
Jesicca1 <105424817+Jesicca1@users.noreply.github.com>
2022-08-31 21:09:52 +0200

    Add frontend-only validations of ResetPasswortModal

---
+++ Models/DTO/ResetPassword.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models.DTO;
+
+public class ResetPassword
+{
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+    public string NewPassword { get; set; }
+
+    [Required]
+    [CompareProperty(nameof(NewPassword))]
+    public string ConfirmPassword { get; set; }
+}
