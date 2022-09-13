cc961275c84060046bee8571712f5759f8ad3c51
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-09-13 17:19:51 +0200

    Stupid warning suppression

--- Backend/AspPlugins/AtriaControllerBase.cs
+++ Backend/AspPlugins/AtriaControllerBase.cs
@@ -4,9 +4,11 @@ namespace Backend.AspPlugins;
 
 public class AtriaControllerBase : ControllerBase {
     [Obsolete("Use Forbidden", true)]
+#pragma warning disable CS0809 // Obsolete member overrides non-obsolete member
     public override ForbidResult Forbid() => throw new NotImplementedException();
     [Obsolete("Use Forbidden", true)]
     public override ForbidResult Forbid(params string[] _) => throw new NotImplementedException();
+#pragma warning restore CS0809 // Obsolete member overrides non-obsolete member
 
     public ActionResult Forbidden() => StatusCode(403);
     public ActionResult Forbidden(object? obj) => StatusCode(403, obj);
