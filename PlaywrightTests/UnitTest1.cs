1e8403aadfa64314d1dc7a0eab90569446f586aa
2hyunz <74467584+uwlnf@users.noreply.github.com>
2022-09-18 19:51:36 +0200

    add login test

--- PlaywrightTests/UnitTest1.cs
+++ PlaywrightTests/UnitTest1.cs
@@ -4,16 +4,20 @@ namespace PlaywrightTests;
 
 public class Tests {
 
-    [Test]
-    public async Task RegisterAndLogout() {
+    public IBrowser Browser { get; set; }
+    private readonly string _url = "https://localhost:7206/";
 
+    [OneTimeSetUp]
+    public async Task SetupBrowser() {
         using var playwright = await Playwright.CreateAsync();
-        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions {
-            Headless = false
-        });
-        var page = await browser.NewPageAsync();
+        Browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions {Headless = false});
+    }
 
-        await page.GotoAsync("https://localhost:7206/");
+    [Test]
+    public async Task RegisterAndLogoutTest() { 
+        var page = await Browser.NewPageAsync();
+
+        await page.GotoAsync(_url);
         await page.Locator("text=Registrieren").ClickAsync();
 
         await page.Locator("#lastName").FillAsync("Mustermann");
@@ -23,10 +27,25 @@ public class Tests {
         await page.Locator("#confirmPassword").FillAsync("12345");
 
         await page.Locator("button:has-text(\"Registrieren\")").ClickAsync();
-        await page.WaitForURLAsync("https://localhost:7206/");
+        await page.WaitForURLAsync(_url);
 
         await page.Locator("[aria-label=\"person circle outline\"]").ClickAsync();
         await page.Locator("text=Abmelden").ClickAsync();
-        await page.WaitForURLAsync("https://localhost:7206/");
+        await page.WaitForURLAsync(_url);
     }
+
+    [Test]
+    public async Task LoginTest() {
+        var page = await Browser.NewPageAsync();
+
+        await page.GotoAsync(_url);
+        await page.Locator("text=Einloggen").ClickAsync();
+
+        await page.Locator("input[type=\"email\"]").FillAsync("testemail@mail.com");
+        await page.Locator("#password").FillAsync("12345");
+
+        await page.Locator("button:has-text(\"Anmelden\")").ClickAsync();
+        await page.WaitForURLAsync(_url);
+    }
+
 }
