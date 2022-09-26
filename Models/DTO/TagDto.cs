3fb1dd0327d3bdd69d56311469c18d4a7d021d35
JohHub <j.a.huber@t-online.de>
2022-09-26 14:29:44 +0200

    comma seperated new tag implementation a little more intuitive

--- Models/DTO/TagDto.cs
+++ Models/DTO/TagDto.cs
@@ -3,6 +3,6 @@
 namespace Models.DTO;
 
 public class TagDto {
-    [RegularExpression(@"^[a-zA-Z0-9-.]*(,[a-zA-Z0-9-.]+)*$", ErrorMessage = "Tags are not separated with a comma.")]
+    [RegularExpression(@"^[a-zA-Z0-9-.]*(, [a-zA-Z0-9-.]+)*$", ErrorMessage = "Tags are not separated with a comma.")]
     public string? NewTagsStr { get; set; }
 }
