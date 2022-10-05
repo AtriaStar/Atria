4aa049fbf7c6f9683c3ebdbce5b6e0d2ccef2f32
2hyunz <74467584+uwlnf@users.noreply.github.com>
2022-10-05 20:40:30 +0200

    Add tags to edit wse detail and gaps in collab modal

--- Backend/Migrations/20221005165254_Initial.Designer.cs
+++ Backend/Migrations/20221005165254_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Backend.Migrations
 {
     [DbContext(typeof(AtriaContext))]
-    [Migration("20221002135302_Initial")]
+    [Migration("20221005165254_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
