add4664f01b83ddba11eff8ff48f87a91bc9462c
2hyunz <74467584+uwlnf@users.noreply.github.com>
2022-10-11 00:41:27 +0200

    demo

--- Backend/Migrations/20221008212902_Initial.Designer.cs
+++ Backend/Migrations/20221008212902_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Backend.Migrations
 {
     [DbContext(typeof(AtriaContext))]
-    [Migration("20221007123838_Initial")]
+    [Migration("20221008212902_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
